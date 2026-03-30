f8c7ee5e4bff364c8856ec9452a2c63b6b71ac7d
roro174 <rodolphe.prevot@outlook.be>
2026-03-30 19:56:52 +0400

    début prunning

--- Code/Graph.hpp
+++ Code/Graph.hpp
@@ -9,6 +9,8 @@
 
 using namespace std;
 
+
+
 class Graph {
 private:
     size_t n; // nombre de sommets
@@ -16,29 +18,162 @@ private:
     std::vector<int> adjList;
     std::vector<std::vector<int>> neighbors;
 
+    /**
+     * @brief  
+     * @note   
+     * @param  graph6: 
+     * @retval None
+     */
+
     void buildFromGraph6(const string& graph6);
 
+    /**
+     * @brief  
+     * @note   
+     * @param  i: 
+     * @param  j: 
+     * @retval 
+     */
     int& at(size_t i, size_t j) { return adjList[i*n + j]; }
+
+    /**
+     * @brief  
+     * @note   
+     * @param  i: 
+     * @param  j: 
+     * @retval 
+     */
     int at(size_t i, size_t j) const { return adjList[i*n + j]; }
 
 public:
-    // --- Constructeurs ---
-    Graph(int numVertices, bool directed = false);   
+
+    /**
+     * @brief  
+     * @note   
+     * @param  numVertices: 
+     * @param  directed: 
+     * @retval 
+     */
+    Graph(int numVertices, bool directed = false);
+
+
+    /**
+     * *  */   
     Graph(const string& Graph6, bool directed = false); 
 
+
+    /**
+     * @brief  
+     * @note   
+     * @param  u: 
+     * @param  v: 
+     * @param  weight: 
+     * @retval None
+     */
     void addEdge(size_t u, size_t v, int weight = 1);
+
+
+    /**
+     * @brief  
+     * @note   
+     * @param  u: 
+     * @param  v: 
+     * @retval None
+     */
     void removeEdge(size_t u, size_t v);
+
+
+    /**
+     * @brief  
+     * @note   
+     * @param  u: 
+     * @param  v: 
+     * @retval 
+     */
     bool hasEdge(size_t u, size_t v) const;
+
+
+    /**
+     * @brief  
+     * @note   
+     * @param  v: 
+     * @retval 
+     */
     vector<int> getNeighbors(size_t v) const;
+
+    /**
+     * @brief  
+     * @note   
+     * @retval None
+     */
     void printMatrix() const;
+
+    /**
+     * @brief  
+     * @note   
+     * @retval None
+     */
     void printGraph6() const;
+
+    /**
+     * @brief  
+     * @note   
+     * @retval 
+     */
     std::vector<uint8_t> nGraph6() const;
+
+    /**
+     * @brief  
+     * @note   
+     * @param  x: 
+     * @param  &result: 
+     * @retval None
+     */
     void rGraph6(std::string x, std::vector<uint8_t> &result) const;
+
+    /**
+     * @brief  
+     * @note   
+     * @retval 
+     */
     std::string upperTriangleMatrix() const;
+
+    /**
+     * @brief  
+     * @note   
+     * @param  other: 
+     * @retval 
+     */
     bool operator==(const Graph& other) const;
+
+    /**
+     * @brief  
+     * @note   
+     * @param  other: 
+     * @retval 
+     */
     bool operator!=(const Graph& other) const;
+
+    /**
+     * @brief  
+     * @note   
+     * @retval 
+     */
     int size() const;
+
+    /**
+     * @brief  
+     * @note   
+     * @param  perm: 
+     * @retval 
+     */
     Graph applyPermutation(const std::vector<int> perm) const;
+
+    /**
+     * @brief  
+     * @note   
+     * @retval 
+     */
     std::string toGraph6() const;
 };
 
--- Code/main.cpp
+++ Code/main.cpp
@@ -1,134 +1,107 @@
 #include "Graph.hpp"
 #include "Partition.hpp"
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct NautyResult {
+    Graph canonicalGraph;
+    vector<vector<int>> allPermutations;
+};
 
 
+bool compareLexicoLess(const vector<vector<int>>& a, const vector<vector<int>>& b) { // True si a < b
+    return std::lexicographical_compare(a.begin(),a.begin() + b.size(),b.begin(),b.end());
+    }
 
-void refinementFunction(const Graph& G,Partition& currentPartition,Partition& BetterPartition,vector<vector<int>> &bestInvariant,
-vector<vector<int>> &currentInvariant,vector<vector<int>> &allPermutations) {
+void refinementFunction(const Graph& G,Partition& currentPartition, Partition& BetterPartition,vector<vector<int>> &bestInvariant,
+                        vector<vector<int>> &currentInvariant,vector<vector<int>>* allPermutations){
     currentInvariant.push_back(currentPartition.InvariantTriangleByCell(G));
-    if(currentPartition.isDiscrete()) {
-        allPermutations.push_back(currentPartition.transformCellsToInt());
-        if(!BetterPartition.isDiscrete()) {
+    if (currentPartition.isDiscrete()) {
+        if (allPermutations != nullptr) allPermutations->push_back(currentPartition.transformCellsToInt());
+        if (!BetterPartition.isDiscrete()) {
             BetterPartition = currentPartition;
-            bestInvariant = currentInvariant;
-        }
+            bestInvariant = currentInvariant;}
         else if (bestInvariant < currentInvariant) {
             BetterPartition = currentPartition;
-            bestInvariant = currentInvariant;
-        }
+            bestInvariant = currentInvariant;}
     }
+
+    // Prunning P_a + P_b
+    else if(BetterPartition.isDiscrete() and (!compareLexicoLess(bestInvariant, currentInvariant))) {
+        return;
+    }
+
     else {
-        std::vector<Partition::Cell> alpha;
+        vector<Partition::Cell> alpha;
         Partition::Cell targetCell = currentPartition.targetCellSelector();
-        for(int v : targetCell.verts){
+        for (int v : targetCell.verts) {
             Partition copyPartition = currentPartition;
             copyPartition.individualizeVertex(v);
             alpha.push_back(copyPartition.getCellByVertex(v));
             copyPartition.refineGraph(G, alpha);
             refinementFunction(G,copyPartition,BetterPartition,bestInvariant,currentInvariant,allPermutations);
             alpha.pop_back();
-            currentInvariant.pop_back();
-        }
+            currentInvariant.pop_back();}
     }
 }
 
+NautyResult computeNauty(const Graph& mainGraph, bool storePerms = false) {
+    Partition P(mainGraph.size());
+    vector<Partition::Cell> alpha = P.getCells();
+    P.refineGraph(mainGraph, alpha);
+    Partition BestP = P;
+    vector<vector<int>> bestInv;
+    vector<vector<int>> currInv;
+    vector<vector<int>> allPerms;
+    vector<vector<int>>* ptrPerms = storePerms ? &allPerms : nullptr;
+    refinementFunction(mainGraph,P,BestP,bestInv,currInv,ptrPerms);
+    vector<int> perm = BestP.transformCellsToInt();
+    Graph canonG = mainGraph.applyPermutation(perm);
+    return {canonG, allPerms};
+}
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cout << "Usage:\n";
-        std::cout << "  iso graph6 graph6\n";
-        std::cout << "  canonical graph6\n";
-        return 1;
+        cout << "Usage:\n";
+        cout << "  iso graph6 graph6\n";
+        cout << "  canonical graph6\n";
+        cout << "  all graph6\n";
+        return 1;}
+    string mode = argv[1];
+    if (mode == "iso") {
+        if (argc < 4) {
+            cout << "Usage: iso graph6 graph6\n";
+            return 1;
+        }
+        string g1_str = argv[2];
+        string g2_str = argv[3];
+        auto res1 = computeNauty(Graph(g1_str));
+        auto res2 = computeNauty(Graph(g2_str));
+        if (res1.canonicalGraph.toGraph6() == res2.canonicalGraph.toGraph6()) cout << "Isomorphes" << endl; 
+        else cout << "Non isomorphes" << endl;
     }
-
-    std::string mode = argv[1];
-    if (mode == "iso") { // premier mode comparaison entre 2 graphes
-    if (argc < 4) {
-        std::cout << "Usage: iso graph6 graph6\n";
-        return 1;
-    }
-
-    std::string g1_str = argv[2];
-    std::string g2_str = argv[3];
-    Graph G1(g1_str);
-    Graph G2(g2_str);
-
-    // --- Canonical form de G1 ---
-    Partition P1(G1.size());
-    vector<Partition::Cell> alpha1 = P1.getCells();
-    P1.refineGraph(G1, alpha1);
-    Partition BestP1 = P1;
-    vector<vector<int>> bestInv1;
-    vector<vector<int>> currInv1;
-    vector<vector<int>> allPerms1;
-    refinementFunction(G1, P1, BestP1, bestInv1, currInv1, allPerms1);
-    vector<int> perm1 = BestP1.transformCellsToInt();
-    Graph canonG1 = G1.applyPermutation(perm1);
-
-    // --- Canonical form de G2 ---
-    Partition P2(G2.size());
-    vector<Partition::Cell> alpha2 = P2.getCells();
-    P2.refineGraph(G2, alpha2);
-    Partition BestP2 = P2;
-    vector<vector<int>> bestInv2;
-    vector<vector<int>> currInv2;
-    vector<vector<int>> allPerms2;
-    refinementFunction(G2, P2, BestP2, bestInv2, currInv2, allPerms2);
-    vector<int> perm2 = BestP2.transformCellsToInt();
-    Graph canonG2 = G2.applyPermutation(perm2);
-
-    std ::cout << "Canonical form of G1: ";
-    canonG1.printGraph6();
-
-    std::cout << "Canonical form of G2: ";
-    canonG2.printGraph6();
-
-    // --- Comparaison ---
-    if (canonG1 == canonG2) {
-        std::cout << "Isomorphes ✅" << std::endl;
-    } else {
-        std::cout << "Non isomorphes ❌" << std::endl;
-    }
-}
-else if (mode == "canonical" or mode == "all") {
-    if (argc < 3) {
-        std::cout << "Usage: canonical graph6\n";
-        return 1;
-    }
-    std::string g_str = argv[2];
-    Graph mainGraph(g_str);
-    Partition mainPartition(mainGraph.size());
-    vector<Partition::Cell> alpha = mainPartition.getCells();
-    mainPartition.refineGraph(mainGraph, alpha);
-    Partition BetterPartition = mainPartition;
-    vector<vector<int>> bestInvariant;
-    vector<vector<int>> currentInvariant;
-    vector<vector<int>> allPerms;
-    refinementFunction(mainGraph, mainPartition, BetterPartition, bestInvariant, currentInvariant, allPerms);
-    vector<int> intBetterPartition = BetterPartition.transformCellsToInt();
-    Graph canonicalGraph = mainGraph.applyPermutation(intBetterPartition);
-    canonicalGraph.printGraph6();
-    if(mode == "all") {
-        for (const auto& perm : allPerms) {
-            Graph gPerm = mainGraph.applyPermutation(perm);
-            Partition P(gPerm.size());
-            vector<Partition::Cell> alpha2 = P.getCells();
-            P.refineGraph(gPerm, alpha2);
-            Partition BestP = P;
-            vector<vector<int>> bestInv;
-            vector<vector<int>> currInv;
-            vector<vector<int>> dummyPerms;
-            refinementFunction(gPerm, P, BestP, bestInv, currInv, dummyPerms);
-            vector<int> permCanon = BestP.transformCellsToInt();
-            Graph canon = gPerm.applyPermutation(permCanon);
-            canon.printGraph6();
+    else if (mode == "canonical" || mode == "all") {
+        if (argc < 3) {
+            cout << "Usage: canonical graph6\n";
+            return 1;
+        }
+        string g_str = argv[2];
+        Graph mainGraph(g_str);
+        bool storePerms = (mode == "all");
+        auto result = computeNauty(mainGraph, storePerms);
+        result.canonicalGraph.printGraph6();
+        if (mode == "all") {
+            for (const auto& perm : result.allPermutations) {
+                Graph gPerm = mainGraph.applyPermutation(perm);
+                auto res = computeNauty(gPerm);
+                res.canonicalGraph.printGraph6();
+            }
         }
     }
-}
-    else {
-        std::cout << "Mode inconnu ! Utilise 'iso' ou 'canonical'" << std::endl;
-    }
+
+    else cout << "Mode inconnu ! Utilise 'iso' ou 'canonical' ou 'all'" << endl;
 
     return 0;
 }
